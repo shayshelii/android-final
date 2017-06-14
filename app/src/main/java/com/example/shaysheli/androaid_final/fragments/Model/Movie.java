9e707ec0ddbc6b22415ca431bf461dbf9c4cd628
ShaySheli <shaysheli8@gmail.com>
2017-06-14 13:57:08 +0300

    try stuff

---
+++ app/src/main/java/com/example/shaysheli/androaid_final/fragments/Model/Movie.java
@@ -0,0 +1,14 @@
+package com.example.shaysheli.androaid_final.fragments.Model;
+
+import java.util.Date;
+
+/**
+ * Created by ShaySheli on 09/06/2017.
+ */
+
+public class Movie {
+    String id;
+    String name;
+    Date dateCreated;
+    int rate;
+}
